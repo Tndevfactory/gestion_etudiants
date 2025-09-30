35687e3ee4317f87c448399d069d7a297d6b9569
tndev <tndevfactory@gmail.com>
2025-09-30 09:12:58 +0100

    first init

---
+++ backend/resources/views/home/Accueil.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.HomeLayout')
+
+@section('content')
+
+    <div class="container mx-auto p-4 h-screen">
+        <h1 class="text-3xl font-bold mb-4">Bienvenue Student Management System</h1>
+        <p class="text-lg">This is the home page. Use the navigation bar to explore different sections.</p>
+    </div>
+
+@endsection
